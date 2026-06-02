e5eff26e8d7e8a49f8e0dc22ce19decb0e33243b
Reut-Wasserman <reutb9@gmail.com>
2026-06-02 12:27:51 +0300

    delete print

--- RILseq_analysis_package/annotations_graphs.py
+++ RILseq_analysis_package/annotations_graphs.py
@@ -84,8 +84,6 @@ def RNA1_RNA2_annotations(chr_dic, base_path, experiments, rna_types_excel, anno
             for option2 in options:
                 if (option1, option2) in small_df_pairs:
                     results.loc[option2, option1] = dic[(option1, option2)]
-        print(results)
-        print(os.path.join(output_path, f"{experiment}_{chimeras_or_fragments}.png"))
         for annot in [True, False]:
             plt.figure()
             sns.heatmap(results, annot=annot, cmap='Blues', fmt=".6g", annot_kws={"size": 8})
